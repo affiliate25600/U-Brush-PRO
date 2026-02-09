aae774b664d44a6382edb8fbe9f0eb23b03f10c3
16BitDevelopment <16bitdevelopment@gmail.com>
2026-02-09 20:16:45 +1100

    Last change not needed

--- src/app.js
+++ src/app.js
@@ -241,5 +241,5 @@ app.use((req, res, next) => {
   res.status(404).send("Sorry, the requested page cannot be found!");
 });
 
-const PORT = process.env.PORT || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}`));
